22479ffba1f0b20990d8bbc83ca8927146120946
izzypt <bonvalots@gmail.com>
2023-12-03 15:37:26 +0000

    update on ex00

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -46,4 +46,6 @@ class GradeTooLowException : public std::exception
             }
 };
 
+std::ostream & operator <<(std::ostream &ost, Bureaucrat const &bur);
+
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,15 +6,112 @@
 /*   By: simao <simao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 13:47:25 by simao             #+#    #+#             */
-/*   Updated: 2023/12/02 13:53:36 by simao            ###   ########.fr       */
+/*   Updated: 2023/12/03 13:19:57 by simao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.cpp"
+#include "Bureaucrat.hpp"
 
-int main()
+int main(void)
 {
-    Bureaucrat::Joao("Joao");
+    Bureaucrat bur("Luis", 150);
+    std::cout << bur.getName() << std::endl;
+    std::cout << bur.getGrade() << std::endl;
+
+    std::cout << "--------------------" << std::endl;
+
+    Bureaucrat other("Lucas", 1);
+    std::cout << other.getName() << std::endl;
+    std::cout << other.getGrade() << std::endl;
+
+    std::cout << "-----TO LOW------(150)---------" << std::endl;
+
+    try 
+    {
+        Bureaucrat other_bur("Nico", 151);
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+
+    std::cout << "------TO HIGH-----(0)---------" << std::endl;
+
+    try 
+    {
+        Bureaucrat other_bur("Rute", 0);
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+
+    std::cout << "----------OK----------" << std::endl;
+
+    try 
+    {
+        Bureaucrat other_bur("Daniela", 5);
+        std::cout << other_bur.getName() << std::endl;
+        std::cout << other_bur.getGrade() << std::endl;
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+
+    std::cout << "------DECREMENT--------------" << std::endl;
+
+    try 
+    {
+        bur.decGrade();
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+    std::cout << bur.getGrade() << std::endl;
+
+    std::cout << "-------INCREMENT-------------" << std::endl;
+
+    try 
+    {
+        bur.incGrade();
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+    std::cout << bur.getGrade() << std::endl;
+
+    std::cout << "--------INCREMENET------------" << std::endl;
+
+    try 
+    {
+        other.incGrade();
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+    std::cout << other.getGrade() << std::endl;
+
+    std::cout << "-------DECREMENT-------------" << std::endl;
+
+    try 
+    {
+        other.decGrade();
+    }
+    catch (std::exception &error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+    std::cout << other.getGrade() << std::endl;
+
+    std::cout << "--------------------" << std::endl;
+
+    std::cout << bur << std::endl;
     
-    return 0;
+    std::cout << "--------------------" << std::endl;
+    
+    return (0);
 }
